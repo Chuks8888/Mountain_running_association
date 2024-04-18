91d8cf068db49dbd9c6f8d3e3089a6c359b0608f
Chuks888 <hugotarczynski@gmail.com>
2024-04-18 21:57:19 +0200

    more methods to official class

--- Association.h
+++ Association.h
@@ -3,31 +3,34 @@
 #include <string.h>
 #include <vector>
 #include <map>
+#include <list>
 using namespace std;
-
-struct Mountain;
-class Member;
-class Track;
-class Race;
-class League;
 
 class Association
 {
     private:
 		string Name;
-        vector<Member> members; // list of runners
-        vector<Track> tracks; // list of running tracks
-        vector<Race> races; // list of races (finished, in progress, future)
-		vector<League> leagues; // league is just a bigger event than a signle race that consists of races
-		vector<Mountain> mountains; // list of mountaints
+        map<unsigned int, Member*> members; // map of runners
+        map<unsigned int, Track*> tracks; // map of running tracks
+        map<unsigned int, Race*> races; // map of races (finished, in progress, future)
+		map<unsigned int, League*> leagues; // league is just a bigger event than a signle race that consists of races
+		list<Mountain*> mountains; // list of mountaints
     public:
         Association(string Name);
         ~Association();
-        void Add_Member(Member&); 
-        void Add_Track(Track&);
-        void Add_Race(Race&);
-        void Add_League(League&);
-        void Add_Mountain(Mountain&);
+        // 2 Modes for adding, either have one object ready, or create it manually with normal constructors
+        unsigned int Add_Member(Member&);   unsigned int Add_Member(); // it also returns its id
+        unsigned int Add_Track(Track&);     unsigned int Add_Track();
+        unsigned int Add_Race(Race&);       unsigned int Add_Race();
+        unsigned int Add_League(League&);   unsigned int Add_League();
+        void Add_Mountain(Mountain&); void Add_Mountain();
+        int Print_Unfinished_Races(); int Print_Finished_Races(); int Print_All_Races(); // Prints race id and name, returns number of them
+        void Edit_Race(); // edits the race, e.g. changes track or add/remove runner etc.
+        void Edit_League(); // edits the League, e.g. changes the race order or add/remove runner etc.
+        void Print_Members();
+        void Print_Tracks();
+    private:
+        void Add_Runners(); // adds runners to race or league
 };
 
 struct Mountain
@@ -57,22 +60,17 @@ class Member
     
         void Print() const;
         long Calculate_performance(); // only for gold memembership
-        void Cancel_membership();
-        void Gold_membership();
+        void Cancel_membership(); // makes member not able to take part in races
+        void Gold_membership(); // gets access to performance index
         void Standard_membership();
-        void Add_race(const Race&); // uses operator
-        void Remove_race(const Race&); void Remove_race(const unsigned int); // uses operator
+        unsigned int operator++(); // increases age by one
+        void Add_race(const Race&);
+        void Remove_race(const Race&); void Remove_race(const unsigned int);
         bool Find_race(const Race&) const; bool Find_race(const unsigned int) const;
     private:
-        unsigned int& get_membership(); // gets reference to the Membership parameter
+        unsigned int get_membership() const; // gets Membership parameter
         unsigned int get_id() const; // gets id of member
         bool empty() const; // checks for any participation
-
-        void operator+(const Race&); // adds a race to the map
-        void operator-(const Race&); // removes race from runners map
-        void operator-(const unsigned int); // removes race from runners map
-        unsigned int operator++(); // increases age by one
-        
 };
 
 class Track
@@ -89,13 +87,13 @@ class Track
 		const unsigned int Id;
         const string Name;
 		const Mountain *Where; // On what mountain is the track
-        int Difficulty; // ranges from 1 - 10
-        int Length; // in kilometers
+        unsigned int Difficulty; // ranges from 1 - 10
+        double Length; // in kilometers
 		map<unsigned int, Race*> Schedule; // Schdule of past, present and future races
         double Average_time;
         Best Best_time; // Structure of the track's record time 
     public:
-        Track(string name, Mountain mountain, int length, int difficulty = 5); // normal constructor
+        Track(string name, Mountain mountain, int length, unsigned int difficulty = 5); // normal constructor
         Track(const Track&); // copy constructor
         ~Track();
 
@@ -105,15 +103,11 @@ class Track
         void Remove_race(const Race&); // removing a race may also remove the best runner
         void Remove_race(const unsigned int); // the add and remove method use operators 
 
-        void operator-=(unsigned int); // decreases the length
-        void operator+=(unsigned int); // increases the length
-        void operator++(); // increase the difficulty
-        void operator--(); // decrease the difficulty
+        double operator-=(double); // decreases the length
+        double operator+=(double); // increases the length
+        unsigned int operator++(); // increase the difficulty
+        unsigned int operator--(); // decrease the difficulty
     private:
-        void operator+(const Race&); // adds a race
-        void operator-(const Race&); // removes a race
-        void operator-(const unsigned int); // -||-
-
         void Finish_race(unsigned int); // Used only from object class Race
         Member* search_for_best(); // If the best runner or the race he was in was removed
         double Recalculate_average(); // after a race is finished or destroyed the average time has to change
@@ -142,22 +136,18 @@ class Race
         Member *Winner;
         bool finished;
     public:
-        Race(string Name, Track where);
+        Race(string Name, Track *where);
         Race(const Race&);
         ~Race();
         
         void Print() const; // Displays info
         void Finish_race(); // finishes race and calculates places, winner, avg_time etc.
+
         void Add_runner(const Member&);
         void Remove_runner(const Member&);
         void Remove_runner(const unsigned int);
-
         void operator=(const Track&); // reaassigns the track
     private:
-        void operator+(const Member&); // Add runner
-        void operator-(const Member&); // remove runner
-        void operator-(const unsigned int); // only if finished = 0
-
         const unsigned int get_id();
         bool Is_finished() const; // checks if the race is finished
         void Calculate_average(); // calculates average time
@@ -188,13 +178,9 @@ class League
         void Remove_Race(const Race&); void Remove_Race(const unsigned int);
         void Add_runner(const Member&);
         void Remove_runner(const Member&); void Remove_runner(const unsigned int);
-        void Start_League(); // stops from adding runners and races
+        void Start_League(); // stops from edditing the league
         void Next_stage(); // finishes next race, or finishes league
     private:
-        void operator+(const Race&); // adds a race
-        void operator-(const Race&);  // removes a race
-        void operator+(const Member&); // Add runner
-        void operator-(const Member&); // remove runner 
         void Declare_winner(); // declares winner based on average time in all races
         const unsigned int get_id();
 
